a403dbe6ca47a43f261918bd496e59a894ff7f9a
ipinky77 <ipinky7@gmail.com>
2021-08-09 17:15:53 +0200

    asdf

--- bitstampTools.js
+++
@@ -1,5 +0,0 @@
-const BitstampClient = require("./bitstampClient.js")
-const BitstampServer = require("./bitstampServer.js")
-const BitstampBot = require("./bitstampBot.js")
-
-module.exports = { BitstampClient, BitstampServer, BitstampBot }
