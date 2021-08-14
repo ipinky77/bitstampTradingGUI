cfecfc593f799c8019f2153964b3144ee2f007d1
ipinky77 <ipinky7@gmail.com>
2021-08-14 19:28:29 +0200

    finetuned profiles, now multiple bots are possible

--- bitstampTools.js
+++
@@ -1,6 +0,0 @@
-const version = "2.0.0"
-const BitstampClient = require("./bitstampClient.js")
-const BitstampServer = require("./bitstampServer.js")
-const BitstampBot = require("./bitstampBot.js")
-
-module.exports = { BitstampClient, BitstampServer, BitstampBot }
